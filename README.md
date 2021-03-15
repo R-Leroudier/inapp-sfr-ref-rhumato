e6ecc888964137016d3ffbad4f12f5d9b5c96fbb
Florian <git@florian-a.dev>
2021-03-15 11:32:48 +0100

    Ajout du modèle de 360 medics.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# inapp-sfr-ref-rhumato
+# medics-tool-template
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve or npm start
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### Documentation
+
+https://docs.google.com/document/d/1QRnS4Dzx8e4DGYxQq7rRnbuTYRkf-D4rGQNp6tdVeJQ/edit?usp=sharing
---
+++ src/router/index.ts
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Home from '../views/Home.vue'
+import OtherPage from '../views/OtherPage.vue'
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/other-page',
+    name: 'OtherPage',
+    component: OtherPage
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+    routes,
+    //do NOT user mode: "history" or "abstract", we need hastags
+    //mode: "history",
+    mode: "hash",
+})
+
+export default router
