650248aa1b23d14a5422ce5139cef1d4f94140d6
oriana <oriana.agniel@gmail.com>
2021-03-16 15:01:03 +0100

    ajout du template scores

---
+++ src/components/Template/template.vue
@@ -0,0 +1,61 @@
+<template>
+<div>
+
+<h2 class="title">Polyarthrite rhumatoïde,
+critères ACR/EULAR</h2>
+<p class="description">Critères diagnostics devant une polyarthrite débutante avec des radiographies normales et en l'absence d'un diagnostic d'une autre maladie.</p>
+<span class="line">------</span>
+<br>
+<card class="introduction">test</card>
+<form class="form">
+<h4 class="categorie">Atteintes articulaires</h4>
+<p class="details">On différencie les grosses et petites articulations :- Grosses articulations : épaule, coude, hanche, genou, cheville. - Petites articulations : toutes autres articulations des membres</p>
+<button type="button" class="btn">0 - 1 grosse articulation.</button>
+<button type="button" class="btn validate"> 2 à 10 grosses articulations.</button>
+<button type="button" class="btn cancel">Primary</button>
+<button type="button" class="btn exit">Primary</button>
+
+
+<h5 class="categorie">Sérologie</h5>
+<p class="details">Notes :
+ - FR = Facteur rhumatoïde.
+ - ACPA = Anticorps anti-peptides/protéines citrulliné(e)s.</p>
+<button type="button" class="btn">FR et ACPA négatifs.</button>
+<button type="button" class="btn validate"> FR ou ACPA faiblement positif</button>
+<button type="button" class="btn cancel"> FR ou ACPA fortement positif</button>
+<button type="button" class="btn exit">Primary</button>
+
+<h6 class="categorie">Durée de la synovite</h6>
+
+<button type="button" class="btn">Durée < 6 semaines.</button>
+<button type="button" class="btn validate"> Durée ≥ 6 semaines.</button>
+
+<h7 class="categorie">Marqueurs de l'inflammation</h7>
+
+<button type="button" class="btn">VS et CRP normales.</button>
+<button type="button" class="btn validate"> VS et CRP anormales.</button>
+<br>
+<button type="button" class="btn validate"> calcul</button>
+<button type="button" class="btn validate"> annule</button>
+<br><card class="result">score
+
+</card>
+</form>
+<span class="line"></span>
+
+</div>
+
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+ mounted()
+    {}
+})
+</script>
+
+
+
+<style></style>
