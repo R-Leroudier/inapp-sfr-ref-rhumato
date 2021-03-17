26fca63ef9b5ead237c7380bbab412f92b561caf
Raphael Leroudier <leroudier.raphael@gmail.com>
2021-03-17 12:27:17 +0100

    Asas terminé

--- src/components/scores/RIC/Spondyloarthrite/Asas.vue
+++ src/components/scores/RIC/Spondyloarthrite/Asas.vue
@@ -3,40 +3,91 @@
     <h2 class="title">Spondylarthrite axiale, Critères ASAS</h2>
     <p class="description"></p>
     <span class="line">------</span>
+    <card class="information">
+      Chez un patient présentant une rachialgie ≥ 3 mois dont l'âge du
+      diagnostic est &lsaquo; 45 ans alors on peut classer sa maladie dans la
+      forme spondylarthrite axiale
+    </card>
+
     <form class="form">
       <h4 class="categorie">Sacro-iliite à l'imagerie:</h4>
       <ul>
         <li>
-          <input type="checkbox" value="1" />
+          <input @click="calcChecked()" class="sacro-iliite" type="checkbox" />
           Inflammation fortement évocatrice de sacro-iliite à l'IRM.
         </li>
         <li>
-          <input type="checkbox" value="1" /> Sacro-iliite radiologique stade ≥
-          2 si bilatéral OU ≥ 3 si unilatéral.
+          <input @click="calcChecked()" class="sacro-iliite" type="checkbox" />
+          Sacro-iliite radiologique stade ≥ 2 si bilatéral OU ≥ 3 si unilatéral.
         </li>
       </ul>
 
       <h4 class="categorie">Signes de spondylarthrite:</h4>
       <ul class="form">
-        <li><input type="checkbox" value="1" />Rachialgie inflammatoire.</li>
-        <li><input type="checkbox" value="1" />Arthrite.</li>
-        <li><input type="checkbox" value="1" />Enthésite (talon).</li>
-        <li><input type="checkbox" value="1" />Uvéite.</li>
-        <li><input type="checkbox" value="1" />Dactylite.</li>
-        <li><input type="checkbox" value="1" />Psoriasis.</li>
         <li>
-          <input type="checkbox" value="1" />Maladie de Crohn / Rectocolite
-          Hémorragique.
+          <input @click="calcChecked()" class="choice" type="checkbox" />
+          Rachialgie inflammatoire.
         </li>
-        <li><input type="checkbox" value="1" />Bonne réponse aux AINS.</li>
         <li>
-          <input type="checkbox" value="1" />Antécédent familial de
-          Spondylarthrite.
+          <input
+            @click="calcChecked()"
+            class="choice"
+            type="checkbox"
+          />Arthrite.
         </li>
-        <li><input type="checkbox" value="1" />HLA-B27 positif.</li>
-        <li><input type="checkbox" value="1" />CRP augmentée.</li>
+        <li>
+          <input
+            @click="calcChecked()"
+            class="choice"
+            type="checkbox"
+          />Enthésite (talon).
+        </li>
+        <li>
+          <input @click="calcChecked()" class="choice" type="checkbox" />Uvéite.
+        </li>
+        <li>
+          <input
+            @click="calcChecked()"
+            class="choice"
+            type="checkbox"
+          />Dactylite.
+        </li>
+        <li>
+          <input
+            @click="calcChecked()"
+            class="choice"
+            type="checkbox"
+          />Psoriasis.
+        </li>
+        <li>
+          <input @click="calcChecked()" class="choice" type="checkbox" />Maladie
+          de Crohn / Rectocolite Hémorragique.
+        </li>
+        <li>
+          <input @click="calcChecked()" class="choice" type="checkbox" />Bonne
+          réponse aux AINS.
+        </li>
+        <li>
+          <input
+            @click="calcChecked()"
+            class="choice"
+            type="checkbox"
+          />Antécédent familial de Spondylarthrite.
+        </li>
+        <li>
+          <input @click="calcChecked()" type="checkbox" id="HLA" />HLA-B27
+          positif.
+        </li>
+        <li>
+          <input @click="calcChecked()" class="choice" type="checkbox" />CRP
+          augmentée.
+        </li>
       </ul>
     </form>
+
+    <card class="result">
+      <p>{{ this.result }}</p>
+    </card>
     <span class="line">------</span>
     <span>
       Interprétation : Les critères ASAS (Assessment of SpondyloArthritis
@@ -64,19 +115,35 @@ export default Vue.extend({
   mounted() {},
   data() {
     return {
-      score1: 0,
-      score2: 0,
+      result: "",
     };
   },
   methods: {
-    choice1(number) {
-      this.score1 = number;
-    },
-    choice2(number) {
-      this.score2 = number;
-    },
-    calcTotal() {
-      return this.score1 + this.score2;
+    calcChecked() {
+      let signe_spondylarthite = document.querySelectorAll(".choice");
+      let positif = document.querySelector("#HLA");
+      let sacro_iliite = document.querySelectorAll(".sacro-iliite");
+      let count_signe_spondylarthite = 0;
+      if (signe_spondylarthite.length > 0) {
+        signe_spondylarthite.forEach((x) => {
+          if (x["checked"]) count_signe_spondylarthite++;
+        });
+      }
+      let count_sacro_iliite = 0;
+      if (sacro_iliite.length > 0) {
+        sacro_iliite.forEach((x) => {
+          if (x["checked"]) count_sacro_iliite++;
+        });
+      }
+      if (
+        (count_signe_spondylarthite > 0 && count_sacro_iliite > 0) ||
+        (positif && positif["checked"] && count_signe_spondylarthite > 1) ||
+        (positif && positif["checked"] && count_sacro_iliite > 0)
+      ) {
+        return (this.result = "forme spondylarthrite axiale POSITIVE");
+      } else {
+        return (this.result = "forme spondylarthrite axiale NEGATIVE");
+      }
     },
   },
 });
