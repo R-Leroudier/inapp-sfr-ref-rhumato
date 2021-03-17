f686d529a4845b2fec8a747adb9bd2a299abae7b
Carneiro92 <jorgecarneiro092@gmail.com>
2021-03-17 12:41:22 +0100

    Structure OK

--- src/components/scores/RIC/Spondyloarthrite/Basdai.vue
+++ src/components/scores/RIC/Spondyloarthrite/Basdai.vue
@@ -1,8 +1,53 @@
 <template>
-  <h3>http://medicalcul.free.fr/basdai.html</h3>
+  <div style="display:flex; flex-direction:column; margin-top:10px">
+    <h3>Score Basdai</h3>
+    <span
+      >Pour chaque item, indiquez une valeur de 0 (absent) à 10 (extrême)
+      concernant votre ressenti.</span>
+    <span>1. Où situez-vous votre degré global de fatigue ? </span>
+    <span> Réponse :</span>
+    <input class="inputNumber" type="number" min="0" max="10" size="6"/>
+    <label for="">(0=absent, 10=extrême)</label>
+    <span>2. Où situez-vous votre degré global de douleur au niveau du cou, du dos et des hanches dans le cadre de votre spondylarthrite ankylosante ? </span>
+    <span> Réponse :</span>
+    <input type="number" min="0" max="10" />
+    <label for="">(0=absent, 10=extrême)</label>
+    <span>3. Où situez-vous votre degré de gonflement articulaire en dehors du cou, du dos et des hanches ? </span>
+    <span> Réponse :</span>
+    <input type="number" min="0" max="10" />
+    <label for="">(0=absent, 10=extrême)</label>
+    <span>4. Où situez-vous votre degré de gêne pour les zones sensibles au toucher ou à la pression ? </span>
+    <span> Réponse :</span>
+    <input type="number" min="0" max="10" />
+    <label for="">(0=absent, 10=extrême)</label>
+    <span>5. Où situez-vous votre degré de raideur matinale depuis votre réveil ? </span>
+    <span> Réponse :</span>
+    <input type="number" min="0" max="10" />
+    <label for="">(0=absent, 10=extrême)</label>
+    <span>1. Où situez-vous votre degré global de fatigue ? </span>
+    <span> Réponse :</span>
+    <input type="number" min="0" max="10" />
+    <label for="">(0=absent, 10=extrême)</label>
+  </div>
 </template>
 
+
 <script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Basdai",
+  data() {
+    return {
+      result: null,
+    };
+  },
+  mounted() {},
+});
 </script>
 
-<style></style>
+<style lang="scss">
+  .inputNumber{
+    width: 30px;
+  }
+</style>
--- src/views/ContentPage.vue
+++ src/views/ContentPage.vue
@@ -10,11 +10,13 @@
 import Vue from "vue";
 import DataService from "@/service/DataService";
 import Score1 from "@/components/scores/Score1.vue";
+import Basdai from "@/components/scores/RIC/Spondyloarthrite/Basdai.vue"
 
 export default Vue.extend({
   name: "Home",
   components: {
     Score1,
+    Basdai
   },
   data() {
     return {
