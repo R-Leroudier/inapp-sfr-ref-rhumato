e3b4395215011e351871e45abdcd51732bfb2773
Raphael Leroudier <leroudier.raphael@gmail.com>
2021-03-18 12:27:13 +0100

    dasppr terminé

--- src/components/scores/RIC/DasPpr.vue
+++ src/components/scores/RIC/DasPpr.vue
@@ -1,27 +1,44 @@
 <template>
   <div>
-    <h2 class="title">disease activity score PPR</h2>
+    <h2 class="title">Calcul du score ‘DAS-PPR’</h2>
     <p class="description"></p>
     <span class="line">------</span>
-    <card class="information">
-      Chez un patient présentant une rachialgie ≥ 3 mois dont l'âge du
-      diagnostic est &lsaquo; 45 ans alors on peut classer sa maladie dans la
-      forme spondylarthrite axiale
-    </card>
 
     <form class="form">
-      <label for="weight"> CRP (divisée par 10) : </label>
-      <input v-model="weight" id="weight" type="text" />
-      <label for="size">Durée de la raideur matinale divisée par 10 : </label>
-      <input v-model="size" id="size" type="text" />
-      <label for="size"
-        >Jugement du patient sur l’activité de sa PPR (0 à 10)
-      </label>
-      <input v-model="size" id="size" type="text" />
-      <label for="size"
-        >Jugement du médecin sur l’activité de la PPR (0 à 10)</label
-      >
-      <input v-model="size" id="size" type="text" />
+      <div>
+        <label for="crp_id"> CRP : </label>
+        <input v-model="crp" id="crp_id" type="text" /> ml/l
+      </div>
+      <div>
+        <label for="duration_stiffness_id"
+          >Durée de la raideur matinale :
+        </label>
+        <input
+          v-model="duration_stiffness"
+          id="duration_stiffness_id"
+          type="text"
+        />
+        minutes
+      </div>
+      <div>
+        <label for="patient_judgment"
+          >Jugement du patient sur l’activité de sa PPR :
+        </label>
+        <select v-model="patient_judgment" id="patient_judgment_id" type="text">
+          <option value="0" :key="0">{{ 0 }}</option>
+          <option v-for="n in 10" :value="n" :key="n">{{ n }}</option>
+        </select>
+      </div>
+      <div>
+        <label for="doctor_judgment_id"
+          >Jugement du médecin sur l’activité de la PPR :
+        </label>
+        <select v-model="doctor_judgment" id="doctor_judgment_id" type="text">
+          <option value="0" :key="0">{{ 0 }}</option>
+          <option v-for="t in 10" :value="t" :key="t">{{ t }}</option>
+        </select>
+      </div>
+
       <ul>
         Capacité à lever les bras:
         <li>
@@ -29,7 +46,7 @@
             v-model="arms_up"
             type="radio"
             id="radio4"
-            name="drone"
+            name="arms_up_radio"
             value="3"
           />
           <label for="radio1">impossible</label>
@@ -39,7 +56,7 @@
             v-model="arms_up"
             type="radio"
             id="radio3"
-            name="drone"
+            name="arms_up_radio"
             value="2"
           />
           <label for="radio2"
@@ -51,7 +68,7 @@
             type="radio"
             v-model="arms_up"
             id="radio2"
-            name="drone"
+            name="arms_up_radio"
             value="1"
           />
           <label for="radio2">au niveau de la ceinture scapulaire (90°)</label>
@@ -61,13 +78,13 @@
             v-model="arms_up"
             type="radio"
             id="radio1"
-            name="drone"
+            name="arms_up_radio"
             value="0"
           />
           <label for="radio4">bien au dessus des épaules (&rsaquo; 90°) </label>
         </li>
       </ul>
-      <input type="submit" @click="getScore()" value="Valider" />
+      <input type="submit" @click="getResult()" value="Valider" />
     </form>
     <card class="result">
       <p>{{ this.result }}</p>
@@ -83,23 +100,32 @@ export default Vue.extend({
   mounted() {},
   data() {
     return {
-      crp: null,
-      duration_stiffness: null,
-      Patient_judgment: null,
-      Patient_doctor: null,
-      arms_up: null,
+      crp: 0,
+      duration_stiffness: 0,
+      patient_judgment: 0,
+      doctor_judgment: 0,
+      arms_up: 0,
       score: 0,
+      result: "",
     };
   },
   methods: {
     getResult() {
       // a revoir la condtion
-      if (this.crp == 30) {
-        this.score = this.score + 3;
-      }
-      if (this.duration_stiffness >= 60) {
-        this.score = this.score + 6;
-      }
+
+      if (this.patient_judgment)
+        this.score = this.score + this.patient_judgment;
+      if (this.doctor_judgment)
+        this.score =
+          this.crp / 10 +
+          this.duration_stiffness / 10 +
+          this.patient_judgment +
+          this.doctor_judgment +
+          this.arms_up;
+      if (this.score >= 7 && this.scrore <= 17) this.result = "Activité modéré";
+      else if (this.score > 17) this.result = "Activité forte";
+      else this.result = "Activité faible";
+      return this.result;
     },
   },
 });
