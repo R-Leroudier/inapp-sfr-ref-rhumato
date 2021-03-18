d565f654c795ae856f4a3514bc3df45ef90451a0
oriana <oriana.agniel@gmail.com>
2021-03-18 12:34:35 +0100

    score asdas

--- src/components/scores/RIC/Spondyloarthrite/Asdas.vue
+++ src/components/scores/RIC/Spondyloarthrite/Asdas.vue
@@ -1,10 +1,88 @@
 <template>
-  <h3>http://medicalcul.free.fr/asdas.html</h3>
+<div>
+  <h2 class="title">Score ASDAS</h2>
+<span class="line">------</span>
+
+<p class="introduction">Renseignez chaque item. Indiquez une valeur de 0 (absent) à 10 (extrême) pour les questions concernant votre ressenti.</card>
+
+<section>
+  <h5>
+    1. Où situez-vous votre degré global de douleur au niveau du cou, du dos et des hanches 
+    dans le cadre de votre spondylarthrite ankylosante ?
+  </h5>
+  <input type="number" id="toggle" class="visually-hidden">
+  <p>(0=absent, 10=extrême)</p>
+</section>
+
+<section>
+  <h5>2. Quelle est la durée de votre raideur matinale à partir de votre réveil ?</h5>
+  <div>
+  <input type="radio">
+  <label>Aucune</label>
+  </div>
+
+  <div>
+  <input type="radio">
+  <label>Aucune</label>
+  </div>
+
+  <div>
+  <input type="radio">
+  <label>15min</label>
+  </div>
+
+  <div>
+  <input type="radio">
+  <label>30min</label>
+  </div>
+
+  <div>
+  <input type="radio">
+  <label>45min</label>
+  </div>
+
+  <div>
+  <input type="radio">
+  <label>1h</label>
+  </div>
+
+  <div>
+  <input type="radio">
+  <label>1h15</label>
+  </div>
+
+  <div>
+  <input type="radio">
+  <label>1h30</label>
+  </div>
+
+  <div>
+  <input type="radio">
+  <label>1h45</label>
+  </div>
+
+  <div>
+  <input type="radio">
+  <label>2h ou plus</label>
+  </div>
+</section>
+
+
+
+</div>
 </template>
 
 <script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+ name:"Asdas",
+ mounted()
+    {}
+})
 </script>
 
-<style></style>
+<style>
+</style>
 
 
