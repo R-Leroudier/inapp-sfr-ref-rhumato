d2b64bdf1e6c81ad507d53cf74c44ed9db3e89fe
oriana <oriana.agniel@gmail.com>
2021-03-19 14:23:19 +0100

    score caspar. Algo Ok

--- src/components/scores/RIC/Caspar.vue
+++ src/components/scores/RIC/Caspar.vue
@@ -1,86 +1,107 @@
 <template>
   <div>
-    <h2 class="title">Critères de classification de CASPAR pour le rhumatisme psoriasique</h2>
+    <h2 class="title">
+      Critères de classification de CASPAR pour le rhumatisme psoriasique
+    </h2>
     <form>
       <section>
-        <p>1. Atteinte rhumatologique inflammatoire (périphérique, axiale ou enthésitique)</p>
+        <p>
+          1. Atteinte rhumatologique inflammatoire (périphérique, axiale ou
+          enthésitique)
+        </p>
         <div>
           <label class="btn">
-          <input type="radio" value="yes" v-model="isFirstCriteriaIsChecked"/>
-          Oui
+            <input type="checkbox" v-on:change="update(0)" v-model="isFirstCriteriaChecked"/>
+            Oui
           </label>
-      </div>
+        </div>
       </section>
 
       <section>
-        <p>2. Présence explicite de psoriasis à l’examen ou dans les antécédents :</p>
+        <p>
+          2. Présence explicite de psoriasis à l’examen ou dans les antécédents
+          :
+        </p>
         <div>
-        <label class="btn">
-          <input type="checkbox" v-model="checkedValues" value="value.a1"/>
-          lésion psoriasique cutanée ou du scalp diagnostiquée par un médecin à l’examen physique
-        </label>
-      </div>
-      <div>
-        <label class="btn">
-          <input type="checkbox" v-model="checkedValues" value="value.a2"/>
-          Notion de psoriasis selon le patient ou un médecin
-        </label>
-      </div>
-      <div>
-        <label class="btn">
-          <input type="checkbox" v-model="checkedValues" value="value.a3"/>
-          Antécédent familial de psoriasis au 1er ou 2nd degré
-        </label>
-      </div>
+          <label class="btn">
+            <input type="checkbox" v-on:change="update(1)" />
+            lésion psoriasique cutanée ou du scalp diagnostiquée par un médecin
+            à l’examen physique
+          </label>
+        </div>
+        <div>
+          <label class="btn">
+            <input type="checkbox" v-on:change="update(2)" />
+            Notion de psoriasis selon le patient ou un médecin
+          </label>
+        </div>
+        <div>
+          <label class="btn">
+            <input type="checkbox" v-on:change="update(3)" />
+            Antécédent familial de psoriasis au 1er ou 2nd degré
+          </label>
+        </div>
       </section>
 
       <section>
         <p>3. Atteinte unguéale</p>
         <div>
-        <label class="btn">
-          <input type="checkbox" v-model="checkedValues" value="value.a4"/>
-          Dystrophie unguéale psoriasique à l’examen clinique : onycholyse, ongles ponctués ou hyperkératose
-        </label>
-      </div>       
+          <label class="btn">
+            <input type="checkbox" v-on:change="update(4)" />
+            Dystrophie unguéale psoriasique à l’examen clinique : onycholyse,
+            ongles ponctués ou hyperkératose
+          </label>
+        </div>
       </section>
 
       <section>
         <p>4. Négativité du facteur rhumatoïde</p>
         <div>
-        <label class="btn">
-          <input type="checkbox" v-model="checkedValues" value="value.a5"/>
-          Absence de FR sérique (ELISA ou néphélométrie)
-        </label>
-      </div>      
+          <label class="btn">
+            <input type="checkbox" v-on:change="update(5)" />
+            Absence de FR sérique (ELISA ou néphélométrie)
+          </label>
+        </div>
       </section>
 
       <section>
         <p>5. Présence ou antécédent de dactylite</p>
         <div>
-        <label class="btn">
-          <input type="checkbox" v-model="checkedValues" value="value.a6"/>
-          Dactylite actuelle diagnostiquée par un médecin
-        </label>
-      </div>
-      <div>
-        <label class="btn">
-          <input type="checkbox" v-model="checkedValues" value="value.a7"/>
-          Antécédent de dactylite constatée par un médecin
-        </label>
-      </div>      
+          <label class="btn">
+            <input type="checkbox" v-on:change="update(6)" /> Dactylite
+            actuelle diagnostiquée par un médecin
+          </label>
+        </div>
+        <div>
+          <label class="btn">
+            <input type="checkbox" v-on:change="update(7)" />
+            Antécédent de dactylite constatée par un médecin
+          </label>
+        </div>
       </section>
 
       <section>
         <p>6.Aspect radiologique de construction osseuse</p>
         <div>
-        <label class="btn">
-          <input type="checkbox" v-model="checkedValues" value="value.a8"/>
-          Présence de signes radiographiques de construction osseuse juxta-articulaire (radiographies des mains et des pieds)
-        </label>
-      </div>
-        
+          <label class="btn">
+            <input type="checkbox" v-on:change="update(8)" />
+            Présence de signes radiographiques de construction osseuse
+            juxta-articulaire (radiographies des mains et des pieds)
+          </label>
+        </div>
       </section>
+      <button type="button" class="btn validate" v-on:click="calcScore">
+        calculer
+      </button>
     </form>
+    <p>
+      Pour retenir le diagnostic: Il faut le critère 1 + au moins 3 points
+      (Se:91.4%, Sp: 98.7%).
+    </p>
+    <div class="result">
+      <p>{{ score }}</p>
+      <p>{{ interpretation }}</p>
+    </div>
   </div>
 </template>
 
@@ -88,35 +109,41 @@
 import Vue from "vue";
 
 export default Vue.extend({
-  name:"Caspar",
+  name: "Caspar",
   mounted() {},
-  data()
-  {
-    return{
-      isFirstCriteriaChecked:false,
-      checkedValues:[],
-      values:
-      {
-        a1:2,
-        a2:1,
-        a3:1,
-        a4:1,
-        a5:1,
-        a6:1,
-        a7:1,
-        a8:1
-      },
-    }
+  data() {
+    return {
+      isFirstCriteriaChecked: false,
+      check: [false, false, false, false, false, false, false, false, false],
+      values: [0, 2, 1, 1, 1, 1, 1, 1, 1],
+      interpretation: "",
+      score: 0,
+    };
   },
-  methods:
-  {
-    calcScore()
-    {
-
+  methods: {
+    update(key) {
+      this.check[key] = !this.check[key];
     },
-  }
-})
-
+    calcScore() {
+      let count = 0;
+      for (let index = 0; index < this.check.length; index++) {
+        if (this.check[index]) {
+          count += this.values[index];
+        }
+      }
+      this.score = count;
+      this.getInterpretation();
+    },
+    getInterpretation() {
+      if (this.isFirstCriteriaChecked == true && this.score >= 3) {
+        this.interpretation =
+          "rhumatisme psoriasique POSITIF (Se:91.4%, Sp: 98.7%)";
+      } else {
+        this.interpretation = "rhumatisme psoriasique NEGATIF";
+      }
+    },
+  },
+});
 </script>
 
 <style></style>
