c636f1c56a3ca95de062ca98a17f192a745add93
Carlos <goniti@live.fr>
2021-07-29 11:02:13 +0200

    clean scss for component ListPage

--- src/components/ListPage.vue
+++ src/components/ListPage.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="ListPage">
-      <div class="list" v-for="node0 in nodes0" :key="node0.name">
-        <router-link :to="'/sublist/' + node0.slug"
+      <div class="ListPage__items" v-for="node0 in nodes0" :key="node0.name">
+        <router-link :to="'/sublist/' + node0.slug" class="ListPage__item"
           >{{ node0.name }}
-          <i class="fas fa-chevron-down" />
+          <i class="fas fa-chevron-down ListPage__item__icon" />
         </router-link>
       </div>
   </main>
@@ -24,53 +24,32 @@ export default Vue.extend({
       .then(() => {
         this.nodes0 = DataService.$data.tree;
       })
-      .catch((e) => {
-        console.log(e);
-      });
   },
 });
 </script>
+
 <style scoped lang="scss">
-@import 'src/sass/global.scss';
 .ListPage {
   display: flex;
   flex-flow: column nowrap;
   padding: 0 0.75rem;
-
-  h1 {
-    padding: 0;
-    margin-bottom: 0;
-    flex-shrink: 0;
-    width: 100%;
-    max-width: 100%;
-    font-size: calc(1.375rem + 1.5vw);
-    font-weight: 500;
-    line-height: 1.2;
-    text-align: left;
-    color: #472e5a;
-  }
-}
-
-.list {
-  a {
-    all: unset;
-    color: #472e5a;
-    font-weight: bold;
-    background: #ecf1f5;
-    border-radius: 5px;
-    display: flex;
-    margin-top: 0.5rem;
-    padding: 16px;
-    cursor: pointer;
-    text-align: left;
-    justify-content: space-between;
-    align-items: center;
-
-    i {
+  &__item {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-top: 0.5rem;
+      padding: 1em;
+      color: #472e5a;
+      background: #ecf1f5;
+      border-radius: 5px;
+      font-weight: 700;
+      text-decoration: none;
+    &__icon{
       background: #80cc28;
       color: #ecf1f6;
       border-radius: 0.5rem;
       padding: 0.5em;
+      margin-left: 1em;
     }
   }
 }
