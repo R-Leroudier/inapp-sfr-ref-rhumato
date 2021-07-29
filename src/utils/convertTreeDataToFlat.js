75615f6c19de9abd293de5e2a9500b7cf53ec940
Carlos <goniti@live.fr>
2021-07-29 16:26:37 +0200

    modify data

--- src/utils/convertTreeDataToFlat.js
+++ src/utils/convertTreeDataToFlat.js
@@ -1,30 +1,29 @@
-// Import
 const fs = require("fs-extra");
 const data = require("../../public/dataTree.json");
+const PATHOFJSONWRITE = `./dataFlat.json`;
 
-let flatEntries = [];
-/**
- * This function convert data nested to data unested
- * @param {Array} entries
- */
-const treeConvertedToFlat = entries => {
-  for (let i = 0; i < entries.length; i++) {
-    const entry = entries[i];
-    let children = entry.children;
-
-    if (children) {
-      treeConvertedToFlat(children);
-      children = [];
-      flatEntries.push(entry);
-    } else {
-      children = [];
-      flatEntries.push(entry);
-    }
-  }
-  return flatEntries;
+const flatData = [];
+const flattenData = (nestedObject, fullObject) => {
+  ConvertToFlat(nestedObject);
+  fillFlatData(fullObject);
 };
 
-const dataConverted = treeConvertedToFlat(data.tree);
+const fillFlatData = flatEntries => {
+  flatEntries.children = [];
+  if (flatData.includes(flatEntries)) {
+    return;
+  }
+  flatData.push(flatEntries);
+};
 
-fs.writeJsonSync(`./dataFlat.json`, JSON.stringify(dataConverted, null, "/t"));
-console.log(typeof dataConverted);
+const ConvertToFlat = treeItems => {
+  treeItems.forEach(item =>
+    item.children ? flattenData(item.children, item) : fillFlatData(item)
+  );
+  return flatData;
+};
+
+const dataConverted = ConvertToFlat(data.tree);
+console.log(dataConverted)
+fs.writeJsonSync(PATHOFJSONWRITE, dataConverted);
+console.info(`json generate in ${PATHOFJSONWRITE}`);
